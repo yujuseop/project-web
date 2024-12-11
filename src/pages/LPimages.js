9516b8371568db05fdaf99c4cc707fc2589c6d39
Kate Jang <bobaejang1@gmail.com>
2024-12-11 18:05:10 +0900

    LandingPage

---
+++ src/pages/LPimages.js
@@ -0,0 +1,13 @@
+
+const images = {
+    bgimg1: require('../images/bgimg1.png'),
+    bgimg2: require('../images/bgimg2.png'),
+    bgimg3: require('../images/bgimg3.png'),
+    bgimg4: require('../images/bgimg4.png'),
+    screen1: require('../images/screen1.png'),
+    screen2: require('../images/screen2.png'),
+    screen3: require('../images/screen3.png'),
+  };
+  
+  export default images;
+  
