7eef0bea9ecb62a1c70223c6dcf936962302f3eb
nagyum <wildanimal19@naver.com>
2024-12-11 18:01:59 +0900

    chore:아이돌 차트 api 받아오고 페이지에 보여주기

---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+const BASE_URL = "https://fandom-k-api.vercel.app/";
+
+export async function getData({ gender, pageSize = 10 }) {
+  const url = new URL(`12-9/charts/{gender}`, BASE_URL);
+  url.searchParams.append("gender", gender);
+  url.searchParams.append("pageSize", pageSize);
+  const response = await fetch(url.toString());
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data: ${response.statusText}`);
+  }
+  return response.json();
+}
--- src/components/MonthList/MonthsList.jsx
+++ src/components/MonthList/MonthsList.jsx
@@ -1,5 +1,65 @@
+import { useState, useEffect } from "react";
+import { getData } from "../../api";
+import CustomButton from "../CustomButtom/CustomButton";
+import chartIMG from "../../assets/icons/Chart.svg";
+import styles from "./styled.module.scss";
+import IdolChart from "./components/IdolChart";
+
 function MonthsList() {
-  return <div>이달의 차트</div>;
+  const [idolList, setIdolList] = useState([]);
+  const [gender, setGender] = useState("female");
+  const [pageSize, setPageSize] = useState(10);
+  const [loading, setLoading] = useState(false); // 로딩 상태 추가
+  const [error, setError] = useState(null); // 에러 상태 추가
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true); // 데이터 로딩 시작
+      try {
+        const data = await getData({ gender, pageSize });
+        return data;
+      } catch (err) {
+        console.error("데이터를 가져오는 데 실패했습니다:", err); // 에러 로그 추가
+        setError("데이터를 가져오는 데 실패했습니다."); // 에러 메시지 설정
+      }
+      // finally {
+      //   setLoading(false); // 데이터 로딩 끝
+      // }
+    };
+
+    fetchData().then((res) => {
+      console.log("res", res);
+      setIdolList(res?.idols);
+      setError(null); // 에러 초기화
+    });
+  }, [gender, pageSize]);
+
+  return (
+    <>
+      <div className={styles.Chart}>
+        <h2>이달의 차트</h2>
+        <CustomButton className={styles.chartBtn}>
+          <img src={chartIMG} />
+          차트 투표하기
+        </CustomButton>
+
+        <ul>
+          {idolList?.map((idol, index) => {
+            console.log("idol", idol);
+            return (
+              <IdolChart
+                key={`${idol.id}-${index}`}
+                imgUrl={idol.profilePicture}
+                group={idol.group}
+                name={idol.name}
+                totalVotes={idol.totalVotes}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default MonthsList;
